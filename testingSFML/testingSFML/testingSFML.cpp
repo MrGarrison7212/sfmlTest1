70b24e6dc4ab8ca4748e851bdc7d1ed1daa34d32
MrGarrison7212 <bokipfc1992@gmail.com>
2022-09-02 17:23:07 +0200

    First example - osnovni primer.

--- testingSFML/testingSFML/testingSFML.cpp
+++ testingSFML/testingSFML/testingSFML.cpp
@@ -7,17 +7,25 @@
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Works");
+	sf::RenderWindow window(sf::VideoMode(300, 300), "SFML Works");
+
+	sf::RectangleShape shape(sf::Vector2f(150,150));
+
+	shape.setFillColor(sf::Color::Blue);
 
 	while (window.isOpen()) {
 
 		sf::Event event;
 		while (window.pollEvent(event)) {
 
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
 
 		}
 
 		window.clear();
+		window.draw(shape);
 		window.display();
 	}
 
