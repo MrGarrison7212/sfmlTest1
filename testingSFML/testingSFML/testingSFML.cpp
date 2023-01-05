f0f91b54d7460f20c78f35f82325558f88e52e22
MrGarrison7212 <bokipfc1992@gmail.com>
2023-01-05 01:46:56 +0100

    TestVersion-6

--- testingSFML/testingSFML/testingSFML.cpp
+++ testingSFML/testingSFML/testingSFML.cpp
@@ -14,29 +14,14 @@ int main()
 
 	shape.setFillColor(sf::Color::Blue);
 
-	bool jump = false;
 
 	while (window.isOpen()) {
 
 		sf::Event event;
 		while (window.pollEvent(event)) {
 
-			//			if (event.type == sf::Event::Closed) {
-			//				window.close();
-			//			}
-
-			switch (event.type) {
-			case sf::Event::Closed:
+			if (event.type == sf::Event::Closed) {
 				window.close();
-				break;
-			case sf::Event::KeyPressed:
-				if (event.key.code == sf::Keyboard::Space)
-					jump = true;
-				break;
-			case sf::Event::KeyReleased:
-				if (event.key.code == sf::Keyboard::Space)
-					jump = true;
-				break;
 			}
 
 		}
